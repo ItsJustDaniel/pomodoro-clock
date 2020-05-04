723f72dfca4f5daac1a121b6d3d58327c9689a29
Daniel Li <danielli12750@gmail.com>
2020-05-05 06:22:04 +1000

    Updates

---
+++ precache-manifest.6d503d2d71120d7fa6c81df3da17fdc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bc00c8a011cc00d9afbf6f219edca0",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "a83c4bedebd402f70d52",
+    "url": "/pomodoro-clock/static/css/main.26453056.chunk.css"
+  },
+  {
+    "revision": "b0e9bcbbafd254469090",
+    "url": "/pomodoro-clock/static/js/2.32223c38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.32223c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83c4bedebd402f70d52",
+    "url": "/pomodoro-clock/static/js/main.753338f6.chunk.js"
+  },
+  {
+    "revision": "c8e805114a65b007e849",
+    "url": "/pomodoro-clock/static/js/runtime-main.8a5682fa.js"
+  }
+]);
